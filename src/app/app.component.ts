6281134a0aca17ae505035ddbd552eacda7cca78
Pedro Ramirez <pedroramirez@Pedros-MacBook-Pro.local>
2024-02-06 23:21:28 -0600

    add my own bootstrap angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//how to import ng-bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-bootstrap-app';
+  active: string = ''; // Initialize the 'active' property
 }
